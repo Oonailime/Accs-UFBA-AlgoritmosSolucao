77ae7c0004455379abf42b5aad847bbf64035a43
Oonailime <emiliano.guimaraes@ufba.br>
2023-06-22 02:43:37 -0300

    indentado automaticamente

--- judo.cpp
+++ judo.cpp
@@ -4,49 +4,74 @@
 #include <algorithm>
 using namespace std;
 
-struct Atleta {
+struct Atleta
+{
     string nome;
     float peso;
     string categoria;
 };
 
-bool compararPorPeso(Atleta& a, Atleta& b) {
+bool compararAtleta(const Atleta &a, const Atleta &b)
+{
+    if (a.peso == b.peso)
+    {
+        return a.nome < b.nome;
+    }
     return a.peso < b.peso;
 }
 
-string determinarCategoria(float peso) {
-    if (peso <= 44) {
+string determinarCategoria(float peso)
+{
+    if (peso <= 44)
+    {
         return "Super-ligeiro";
-    } else if (peso <= 48) {
+    }
+    else if (peso <= 48)
+    {
         return "Ligeiro";
-    } else if (peso <= 52) {
+    }
+    else if (peso <= 52)
+    {
         return "Meio-leve";
-    } else if (peso <= 57) {
+    }
+    else if (peso <= 57)
+    {
         return "Leve";
-    } else if (peso <= 63) {
+    }
+    else if (peso <= 63)
+    {
         return "Meio-medio";
-    } else if (peso <= 70) {
+    }
+    else if (peso <= 70)
+    {
         return "Medio";
-    } else if (peso <= 78) {
+    }
+    else if (peso <= 78)
+    {
         return "Meio-pesado";
-    } else {
+    }
+    else
+    {
         return "Pesado";
     }
 }
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
     vector<Atleta> atletas(n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> atletas[i].nome >> atletas[i].peso;
     }
 
-    sort(atletas.begin(), atletas.end(), compararPorPeso);
+    stable_sort(atletas.begin(), atletas.end(), compararAtleta);
 
-    for (int i=0;i<n;i++) {
+    for (int i = 0; i < n; i++)
+    {
         atletas[i].categoria = determinarCategoria(atletas[i].peso);
         cout << atletas[i].nome << " " << atletas[i].categoria << endl;
     }
