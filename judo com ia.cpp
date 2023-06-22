0169691eb69df74fb04d22019ed6e753088a0569
Oonailime <emiliano.guimaraes@ufba.br>
2023-06-22 02:42:44 -0300

    arrumei o judo

--- judo com ia.cpp
+++ judo com ia.cpp
@@ -1,89 +1,54 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-struct Atleta
-{
-    string nome;
+struct Atleta {
     int peso;
-    string categoria;
+    string nome;
 };
 
-string determinarCategoria(int peso)
-{
-    if (peso <= 44)
-    {
+bool compararPorPeso( Atleta a,  Atleta b) {
+    return a.peso < b.peso;
+}
+
+string determinarCategoria(int peso) {
+    if (peso <= 44) {
         return "Super-ligeiro";
-    }
-    else if (peso <= 48)
-    {
+    } else if (peso <= 48) {
         return "Ligeiro";
-    }
-    else if (peso <= 52)
-    {
+    } else if (peso <= 52) {
         return "Meio-leve";
-    }
-    else if (peso <= 57)
-    {
+    } else if (peso <= 57) {
         return "Leve";
-    }
-    else if (peso <= 63)
-    {
+    } else if (peso <= 63) {
         return "Meio-medio";
-    }
-    else if (peso <= 70)
-    {
+    } else if (peso <= 70) {
         return "Medio";
-    }
-    else if (peso <= 78)
-    {
+    } else if (peso <= 78) {
         return "Meio-pesado";
-    }
-    else
-    {
+    } else {
         return "Pesado";
     }
 }
 
-void ordenarAtletas(vector<Atleta> &atletas)
-{
-    int n = atletas.size();
-    for (int i = 0; i < n - 1; i++)
-    {
-        int menorPesoIdx = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (atletas[j].peso < atletas[menorPesoIdx].peso)
-            {
-                menorPesoIdx = j;
-            }
-        }
-        if (menorPesoIdx != i)
-        {
-            swap(atletas[i], atletas[menorPesoIdx]);
-        }
-    }
-}
-
-int main()
-{
+int main() {
     int n;
     cin >> n;
 
-    vector<Atleta> atletas(n);
+    vector<Atleta> atletas;
+    Atleta aux;
 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> atletas[i].nome >> atletas[i].peso;
+    for (int i = 0; i < n; i++) {
+
+        cin >> aux.nome >> aux.peso;
+        atletas.push_back(aux);
     }
 
-    ordenarAtletas(atletas);
+    stable_sort(atletas.begin(), atletas.end(), compararPorPeso);
 
-    for (auto &atleta : atletas)
-    {
-        atleta.categoria = determinarCategoria(atleta.peso);
-        cout << atleta.nome << " " << atleta.categoria << endl;
+    for (int i = 0; i < n; i++) {
+        cout << atletas[i].nome << " " << determinarCategoria(atletas[i].peso) << endl;
     }
 
     return 0;
